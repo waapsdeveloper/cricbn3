6cf0ca35eedd875d182fb7bfd45e5650bb45e94a
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-02-26 18:07:53 +0500

    adasd

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TournamentController;
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +17,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+
+
+Route::group(['prefix' => 'tournaments'], function () {
+    // Index - List all tournaments
+    Route::get('/', [TournamentController::class, 'index']);
+
+    // Store - Store a newly created tournament in the database
+    Route::post('/', [TournamentController::class, 'store']);
+
+    // Update - Update the specified tournament in the database
+    Route::put('/{id}', [TournamentController::class, 'update']);
+
+    // Destroy - Remove the specified tournament from the database
+    Route::delete('/{id}', [TournamentController::class, 'destroy']);
+});
