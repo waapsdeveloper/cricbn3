353e8f63a636beef7578aaae9fe6f5da819a4da6
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-02-28 17:39:23 +0500

    checking mobile-api

---
+++ app/Http/Controllers/MobileApi/DashboardHomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\MobileApi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardHomeController extends Controller
+{
+    //
+
+    public function matches(){
+        return "A";
+    }
+
+}
---
+++ routes/mobile-api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('dashboard')->group(function () {
+
+    Route::prefix('home')->group(function () {
+        Route::get('/matches', 'DashboardHomeController@matches');
+    });
+
+
+    // Add more routes for the dashboard as needed
+});
+
+// Route::prefix('matches')->group(function () {
+//     Route::get('/', 'MatchController@index');
+//     Route::post('/', 'MatchController@store');
+//     Route::get('/{id}', 'MatchController@show');
+//     // Add more routes for matches as needed
+// });
